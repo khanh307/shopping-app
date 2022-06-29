e6f588a3543c165dbfa7f87fafb8b3cd3bfdc06b
khanh307 <tranquockhanh307@gmail.com>
2022-06-29 20:22:33 +0700

    Fix read image from Server

--- app/src/main/java/com/example/shoppe/Activity/MainActivity.kt
+++ app/src/main/java/com/example/shoppe/Activity/MainActivity.kt
@@ -1,5 +1,8 @@
 package com.example.shoppe.Activity
 
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.os.AsyncTask
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.appcompat.app.ActionBarDrawerToggle
@@ -7,7 +10,9 @@ import androidx.core.view.GravityCompat
 import com.example.shoppe.Adapter.ListViewNavigationAdapter
 import com.example.shoppe.Data.NavigationItem
 import com.example.shoppe.R
+import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.activity_main.*
+import java.net.URL
 
 class MainActivity : AppCompatActivity() {
 
@@ -16,9 +21,17 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
         showNavigation()
 
         setAdapterListViewNavigation()
+
+
+        var url = "http://192.168.1.172:8012/shopee/images/ic_app.png"
+        var srl = "https://cdn01.dienmaycholon.vn/filewebdmclnew/public//userupload/images/he-dieu-hanh-android-la-gi-3.jpg"
+        Picasso.get().load(url)
+            .error(R.drawable.ic_battery)
+            .into(imageview)
 
     }
 
@@ -38,4 +51,5 @@ class MainActivity : AppCompatActivity() {
             drawerLayout.openDrawer(GravityCompat.START)
         }
     }
+
 }
