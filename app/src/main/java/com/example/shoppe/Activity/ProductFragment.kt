6afba1d0ad67b66248408225986e761077da433a
khanh307 <tranquockhanh307@gmail.com>
2022-07-07 21:01:01 +0700

    load more data but not working

--- app/src/main/java/com/example/shoppe/Activity/ProductFragment.kt
+++ app/src/main/java/com/example/shoppe/Activity/ProductFragment.kt
@@ -1,20 +1,19 @@
 package com.example.shoppe.Activity
 
 import android.annotation.SuppressLint
+import android.content.Context.LAYOUT_INFLATER_SERVICE
 import android.os.Bundle
-import android.util.Log
+import android.os.Handler
+import android.os.Message
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
-import android.widget.ViewFlipper
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.android.volley.Request
 import com.android.volley.RequestQueue
 import com.android.volley.Response
-import com.android.volley.toolbox.JsonArrayRequest
 import com.android.volley.toolbox.StringRequest
 import com.android.volley.toolbox.Volley
 import com.example.shoppe.Adapter.ProductAdapter
@@ -30,6 +29,11 @@ class ProductFragment : Fragment() {
     var arrayProduct: ArrayList<Product> = ArrayList()
     var page: Int = 1
     lateinit var type: String
+    lateinit var footer: View
+    lateinit var viewMain: RecyclerView
+    var gridLayoutManager = GridLayoutManager(context, 2)
+    lateinit var handler: mHanler
+    var limitdata = false
 
     @SuppressLint("MissingInflatedId")
     override fun onCreateView(
@@ -37,7 +41,10 @@ class ProductFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         var view = inflater.inflate(R.layout.fragment_product, container, false)
-        var recyclerView: RecyclerView = view.findViewById(R.id.viewMain)
+        viewMain = view.findViewById(R.id.viewMain)
+        var inflaterfooter: LayoutInflater = requireContext().getSystemService(LAYOUT_INFLATER_SERVICE) as LayoutInflater
+        footer = inflaterfooter.inflate(R.layout.load_product_layout, null)
+        handler = mHanler()
         var bundle = arguments
         if(bundle != null){
             type = bundle.getString("id", "-1")
@@ -45,10 +52,54 @@ class ProductFragment : Fragment() {
         getData(page)
         adapterProduct = ProductAdapter(requireContext(), arrayProduct)
 
-        recyclerView.setHasFixedSize(true)
-        recyclerView.layoutManager = GridLayoutManager(context, 2)
-        recyclerView.adapter = adapterProduct
+        viewMain.setHasFixedSize(true)
+        viewMain.layoutManager = gridLayoutManager
+        viewMain.adapter = adapterProduct
+
+        loadMoreProduct()
         return view
+    }
+
+    private fun loadMoreProduct() {
+        viewMain.addOnScrollListener(object : RecyclerView.OnScrollListener(){
+            override fun onScrollStateChanged(recyclerView: RecyclerView, newState: Int) {
+
+            }
+
+            override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
+                var visibleItemCount = gridLayoutManager.childCount
+                var totalItemCount = gridLayoutManager.itemCount
+                var firstItem = gridLayoutManager.findFirstVisibleItemPosition()
+                if(firstItem + visibleItemCount == totalItemCount && totalItemCount != 0 && adapterProduct.isLoadingAdd == false && limitdata == false){
+                    adapterProduct.isLoadingAdd = true
+                    var t: ThreadData = ThreadData()
+                    t.start()
+                }
+
+            }
+        })
+    }
+
+    inner class mHanler: Handler() {
+        override fun handleMessage(msg: Message) {
+             if(msg.what == 0){
+                adapterProduct.addFooterLoading()
+             } else {
+                 getData(++page)
+                 adapterProduct.isLoadingAdd = false
+             }
+            super.handleMessage(msg)
+        }
+    }
+
+    inner class ThreadData: Thread(){
+        override fun run() {
+            handler.sendEmptyMessage(0)
+            Thread.sleep(2000)
+            var message: Message = handler.obtainMessage()
+            handler.sendMessage(message)
+            super.run()
+        }
     }
 
 
@@ -57,7 +108,8 @@ class ProductFragment : Fragment() {
         var path: String = Server.pathProduct + page.toString()
 
         var stringRequest: StringRequest =object : StringRequest(Request.Method.POST, path, Response.Listener { response ->
-            if(response != null){
+            if(response != null && response.length > 0){
+                adapterProduct.removeFooterLoading()
                 var jsonArray: JSONArray = JSONArray(response)
                 for(i in 0 until jsonArray.length()){
                     var jsonObject: JSONObject = jsonArray.getJSONObject(i)
@@ -71,6 +123,9 @@ class ProductFragment : Fragment() {
                     arrayProduct.add(Product(id, name, image, price, detail, type))
                     adapterProduct.notifyDataSetChanged()
                 }
+            } else{
+                limitdata = true
+                adapterProduct.removeFooterLoading()
             }
         }, Response.ErrorListener {
 
--- app/src/main/java/com/example/shoppe/Adapter/ProductAdapter.kt
+++ app/src/main/java/com/example/shoppe/Adapter/ProductAdapter.kt
@@ -6,14 +6,20 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
+import android.widget.ProgressBar
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.RecyclerView.ViewHolder
 import com.example.shoppe.Data.Product
 import com.example.shoppe.R
 import com.squareup.picasso.Picasso
 import java.text.DecimalFormat
 
-class ProductAdapter(var context: Context, var arrayProduct: ArrayList<Product>): RecyclerView.Adapter<ProductAdapter.ProductHolder>() {
+class ProductAdapter(var context: Context, var arrayProduct: ArrayList<Product>): RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+
+    private val TYPE_ITEM = 1;
+    private val TYPE_LOADING = 2;
+    var isLoadingAdd: Boolean = false
 
     class ProductHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         var imageView: ImageView
@@ -29,27 +35,69 @@ class ProductAdapter(var context: Context, var arrayProduct: ArrayList<Product>)
 
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ProductHolder {
-        var view: View = LayoutInflater.from(parent.context).inflate(R.layout.layout_product, null)
-        var holder: ProductHolder = ProductHolder(view)
-
-
-        return holder
+    class LoadingViewHolder(itemView: View): RecyclerView.ViewHolder(itemView){
+        var progressBar: ProgressBar
+        init {
+            progressBar = itemView.findViewById(R.id.progressbar)
+        }
     }
 
-    override fun onBindViewHolder(holder: ProductHolder, position: Int) {
-        var item: Product = arrayProduct.get(position)
-        holder.tvName.text = item.name
-        var decimalFormat: DecimalFormat = DecimalFormat("###,###,###");
-        holder.tvPrice.text = decimalFormat.format(item.price)
-        holder.tvDetail.maxLines = 2
-        holder.tvDetail.ellipsize = TextUtils.TruncateAt.END
-        holder.tvDetail.text = item.detail
-        Picasso.get().load(item.image).into(holder.imageView)
+    override fun getItemViewType(position: Int): Int {
+        if(arrayProduct != null && position == arrayProduct.size - 1 && isLoadingAdd){
+            return TYPE_LOADING
+        }
+            return TYPE_ITEM
     }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+       if(viewType == TYPE_ITEM){
+           var view: View = LayoutInflater.from(parent.context).inflate(R.layout.layout_product, null)
+           var holder: ProductHolder = ProductHolder(view)
+           return holder
+       } else{
+           var view: View = LayoutInflater.from(parent.context).inflate(R.layout.layout_product, null)
+           var loadingView: LoadingViewHolder = LoadingViewHolder(view)
+           return loadingView
+       }
+
+    }
+
+//    override fun onBindViewHolder(holder: ProductHolder, position: Int) {
+//        var item: Product = arrayProduct.get(position)
+//        holder.tvName.text = item.name
+//        var decimalFormat: DecimalFormat = DecimalFormat("###,###,###");
+//        holder.tvPrice.text = decimalFormat.format(item.price)
+//        holder.tvDetail.maxLines = 2
+//        holder.tvDetail.ellipsize = TextUtils.TruncateAt.END
+//        holder.tvDetail.text = item.detail
+//        Picasso.get().load(item.image).into(holder.imageView)
+//    }
 
     override fun getItemCount(): Int {
         return arrayProduct.size
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        if(holder.itemViewType == TYPE_ITEM){
+            var item: Product = arrayProduct.get(position)
+            var productHolder = holder as ProductHolder
+            productHolder.tvName.text = item.name
+            var decimalFormat: DecimalFormat = DecimalFormat("###,###,###");
+            productHolder.tvPrice.text = decimalFormat.format(item.price)
+            productHolder.tvDetail.maxLines = 2
+            productHolder.tvDetail.ellipsize = TextUtils.TruncateAt.END
+            productHolder.tvDetail.text = item.detail
+            Picasso.get().load(item.image).into(holder.imageView)
+        }
+    }
+
+    fun addFooterLoading(){
+        isLoadingAdd = true
+    }
+
+    fun removeFooterLoading(){
+        isLoadingAdd = false
+
     }
 
 
